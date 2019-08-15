ea675f6de91c6e261fc669fc6bf4fc83550087e6
wamakimaN <wamamoses@gmail.com>
2019-08-15 12:48:27 +0300

    add initial files for my calculator

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var add = function(number1,number2) {return number1 + number2};
+var number1 =parseInt(prompt("enter a number"));
+var number2 =parseInt(prompt("enter another number"));
+alert(add(number1,number2));
